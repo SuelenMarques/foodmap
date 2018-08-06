b78062a7f2aa45aebb5a740529482c6ac8f278c8
SuelenMarques <39528149+SuelenMarques@users.noreply.github.com>
2018-08-06 03:32:46 -0300

    Começando

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-// Add your JavaScript
+$(document).ready(function () {
+
+  $('#first').delay('2500').fadeOut('slow');
+  $('#fixed').hide().delay('2500').fadeIn('slow');
+
+  });
+
+// $('.filtrar').click(function () {
+//     var inputValue = $('.termo').val();
+
+//     $( "li" ).each(function( ) {
+//       if($(this).text() !== inputValue) {
+//         $(this).fadeOut('slow');
+//       }
+//     });
+//   })
+
+//   $('.data.js').on('input', function () {
+//     if($(this).val() === "") {
+//       $( "restaurantes" ).each(function( ) {
+//         $(this).fadeIn('slow')
+//       });
+//     }
+//   })
+
